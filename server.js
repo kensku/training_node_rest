33427f8d68eac63ad1b15efbdeaf89f01f8e855e
kensku <kensku07@gmail.com>
2022-07-22 20:17:23 -0300

    rotas criadas e funcionando

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
+//Arquivo que cria o servidor inicial
+
 const http = require('http');
 const port = 3000;
 
+
+const rotas = {
+    '/idProf': 'id do professor',
+    '/nomeProf': 'nome do professor',
+    '/emailProf': 'email do professor',
+    '/teleProf': 'telefone do professor',
+    '/disciplina': 'disciplina do professor'
+}
+
+
 const server = http.createServer((req, res)=> {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Curso de Node');
+    res.end(rotas[req.url]);
     
 });
 
