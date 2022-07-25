c1489766ce60c352599cffb58c559e646e9b5a59
kensku <kensku07@gmail.com>
2022-07-25 16:53:54 -0300

    delete method added and working

--- src/app.js
+++ src/app.js
@@ -20,17 +20,45 @@ const professores = [
   },
 ];
 
-//GET nomes do professores em forma de lista JSON
+//GET lista todos professores
 //GET == READ -->
-app.get('/professores',(req, res) =>{
-    res.status(200).json(professores)
+app.get("/professores", (req, res) => {
+  res.status(200).json(professores);
+});
+
+//GET professores por ID
+//GET == READ -->
+app.get("/professores/:id", (req, res) => {
+  let index = findTeacher(req.params.id);
+  res.status(200).json(professores[index]);
 });
 
 //POST == CREATE NEW PROFESSOR
-app.post('/professores', (req, res) =>{
-    //include a new professor to the body
-    professores.push(req.body);
-    res.status(201).send('Professor incluido na lista com sucesso.');
+app.post("/professores", (req, res) => {
+  //include a new professor to the body
+  professores.push(req.body);
+  res.status(201).send("Professor incluido na lista com sucesso.");
 });
+
+//PUT == UPDATE
+app.put("/professores/:id", (req, res) => {
+  let index = findTeacher(req.params.id);
+  professores[index].Disciplina = req.body.Disciplina;
+  res.status(200).json(professores);
+});
+
+//DELETE
+app.delete("/professores/:id", (req, res) => {
+  let { id } = req.params;
+  let index = findTeacher(id);
+  professores.splice(index, 1);
+  res.send(`Professor ${id} removido com sucess`);
+});
+
+//funcao para localizar um professor por ID
+function findTeacher(id) {
+  //checa se o ID do professor é igual ao do parametro passado
+  return professores.findIndex((professor) => professor.id == id);
+}
 
 export default app;
