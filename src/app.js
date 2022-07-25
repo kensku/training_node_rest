543ed3dedb30344be5fbf5f949dab23ff6582dde
kensku <kensku07@gmail.com>
2022-07-25 14:35:08 -0300

    post method created and working

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 const professores = [
   {
@@ -20,8 +21,16 @@ const professores = [
 ];
 
 //GET nomes do professores em forma de lista JSON
+//GET == READ -->
 app.get('/professores',(req, res) =>{
     res.status(200).json(professores)
+});
+
+//POST == CREATE NEW PROFESSOR
+app.post('/professores', (req, res) =>{
+    //include a new professor to the body
+    professores.push(req.body);
+    res.status(201).send('Professor incluido na lista com sucesso.');
 });
 
 export default app;
